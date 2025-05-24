bb6fb5fa209a29da837ec6132131a370d742a72f
ponzu-4C9 <baijingseiya@gmail.com>
2025-05-24 11:19:44 +0900

    報酬システムの実装

--- main.c
+++ main.c
@@ -22,6 +22,12 @@ int com(int x, int y, int z,const double *brain0, int p, double *brain1){
     double input[3] = {(double)x,(double)y,(double)z};
     double output[21];
     matmul(input,brain0,temp,1,3,p);
+    for (int i = 0; i < p; i++)
+    {
+        if(temp[i] < 0){
+            temp[i] = 0;
+        }
+    }
     matmul(temp,brain1,output,1,p,21);
     int max_index = 0;
     for (int i = 0; i < 21; i++)
@@ -37,36 +43,62 @@ int com(int x, int y, int z,const double *brain0, int p, double *brain1){
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-
     // brain生成
-    int numofbrain = 10;
-    double brainA[numofbrain][3*10];
-    for(int i = 0; i < numofbrain; i++)
+    int brainscore = 0;
+    double brainA[3*3];
+    for (int i = 0; i < 3*3; i++)
     {
-        for (int j = 0; j < 3*10; j++)
-        {
-            brainA[i][j] = (double)rand() / RAND_MAX;
-        }
+        brainA[i] = (double)rand() / RAND_MAX;
     }
-    double brainB[numofbrain][10*27];
-    for(int i = 0; i < numofbrain; i++)
+    double brainB[3*27];
+    for (int i = 0; i < 3*27; i++)
     {
-        for (int j = 0; j < 10*27; j++)
-        {
-            brainB[i][j] = (double)rand() / RAND_MAX;
-        }
+        brainB[i] = (double)rand() / RAND_MAX;
     }
-    
     int comhand = com(1,2,3,brainA,10,brainB);
+    int x = 5;
+    int y = 7;
+    int z = 9;
+    printf("%d %d %d\n", x, y, z);
+
     if (1 <= comhand && comhand <= 5){
-        printf("左を%d減らす\n", comhand);
+        printf("COM>左を%d減らす\n", comhand);
+        if( 1 <= comhand && comhand <= x){
+            printf("有効な値\n");
+            x -= comhand;
+            brainscore ++;
+        }else{
+            printf("無効な値\n");
+            brainscore --;
+        }
     }else if (6 <= comhand && comhand <= 12){
-        printf("中央を%d減らす\n", comhand-5);
+        printf("COM>中央を%d減らす\n", comhand-5);
+        if(1 <= comhand-5 && comhand-5 <= y){
+            printf("有効な値\n");
+            y -= (comhand-5);
+            brainscore ++;
+        }else{
+            printf("無効な値\n");
+            brainscore --;
+        }
     }else if (13 <= comhand && comhand <= 21){
-        printf("右を%d減らす\n", comhand-12);
+        printf("COM>右を%d減らす\n", comhand-12);
+        if(1 <= comhand-12 && comhand-12 <= z){
+            printf("有効な値\n");
+            z -= (comhand-12);
+            brainscore ++;
+        }else{
+            printf("無効な値\n");
+            brainscore --;
+        }
     }else{
-        printf("エラー\n");
+        printf("エラー0\n");
     }
+    if (x == 0 && y == 0 && z == 0){
+        printf("COMの勝ち\n");
+        brainscore += 10;
+    }
+    printf("COMのスコア:%d\n", brainscore);
 
     return 0;
 }
